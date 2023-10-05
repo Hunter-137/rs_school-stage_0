6a5193e1c18c91ba9c34957138a9b03eb0578877
Hunter-137 <asrarkulovamal@gmail.com>
2023-10-05 05:39:37 +0500

    feat: html & css prepare

---
+++ js30-3.3-random-game/js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+    const gameArea = document.querySelector('.game-area');
+    const gameBox = document.querySelectorAll('.game-box');
+    
+});
